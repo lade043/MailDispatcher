ca4f0ab6eeaa0d224800433d78dd72b9f1420bb6
Lars Bogner <lars.bogner@outlook.de>
2020-05-23 12:10:37 +0200

    Add general files: -setup.py -requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MailDispatcher',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='GNU GPL v3.0',
+    author='Lars Bogner',
+    author_email='lars.bogner@outlook.de',
+    description='Receives new E-Mails and distributes them to Subprogramms acording to their Subject'
+)
